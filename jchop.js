e407a58d37d28bf568bd7bb8aef5d049b5c9504a
Dennis V <info@devkid.nl>
2015-05-30 18:58:47 +0200

    Fixed IIFE and added docs

--- jchop.js
+++ jchop.js
@@ -19,7 +19,47 @@
 	}
 
 	ext(string, 'cap', function() {
-		return this.charAt(0).toUppercase() + this.slice(1);
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	});
+
+	ext(string, 'rev', function() {
+		return this.split("").reverse().join("");
+	});
+
+	ext(string, 'ins', function(i, str) {
+		return this.substring(0, i) + str + this.substring(i, this.length);
+	});
+
+	ext(string, 'contains', function(s) {
+		return this.indexOf(s) !== -1;
+	});
+
+	ext(string, 'ends', function(s) {
+		return this.indexOf(s, this.length - s.length) !== -1;
+	});
+
+	/** Thanks to SO **/
+	/**
+	  * Format the provided string.
+	  * @function format
+	  * @example "Hello {0}, I like {1}".format("visitor", "javascript")
+	  * @example "Hello {who}, I like {language}".format("visitor", "javascript")
+	  *
+	  */
+	ext(string, 'format', function() {
+		var s = this.toString();
+        if (!arguments.length) {
+            return s;
+        }
+        
+        var args = typeof arguments[0],
+            args = (("string" == args || "number" == args) ? arguments : arguments[0]);
+            
+        for (var a in args) {
+            s = s.replace("{" + a + "}", args[a]);
+        }
+        
+        return s;
 	});
 
 	ext(array, 'contains', function(i) {
@@ -33,4 +73,4 @@
 	ext(number, 'nan', function() {
 		return +this !== +this;
 	});
-}
+}();
